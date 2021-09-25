fc2a8e1ca4fc423e5615d6b593dca344d85d2b66
Joshua Cleland <proxikal@gmail.com>
2021-09-24 22:08:46 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'PKGS=($(cat packages))'
+__hasYes=$(which yes)
+
+# echo "cooper -S ${PKGS[@]}"
+count=0
+for pak in "${PKGS[@]}"
+do
+    if [ -f /usr/bin/$pak ];
+    then
+        # InstallPkgs[#InstallPkgs[@]]=${pak}
+        unset PKGS[$count]
+        # echo "$pak Is already installed"
+    fi
+    ((count=count+1))
+done
 
 
+echo "There Are: ${#PKGS[@]} Items in this array."
+echo "cooper -S ${PKGS[@]}"
+
+# pacman -S ${PKGS[@]}
